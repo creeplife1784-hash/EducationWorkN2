0e6741e857c6bbedd53f491769069bbbd46c7380
Владислав Попов <creeplife1784@gmail.com>
2026-03-01 17:26:02 +0300

    выполнил 4 задание

--- Ex4/Ex4.cpp
+++ Ex4/Ex4.cpp
@@ -12,5 +12,22 @@ int main()
     cout << "Hello World!" << endl;
     cout << " " << endl;
 
-    int number;
+    int height, width;
+
+    cout << "Введите высоту прямоугольника: ";
+    cin >> height;
+    cout << "Введите ширину прямоугольника: ";
+    cin >> width;
+
+    if (height <= 0 || width <= 0) {
+        cout << "Ошибка: высота и ширина должны быть положительными числами!";
+        return 1;
+    }
+
+    for (int row = 0; row < height; ++row){
+        for (int col = 0; col < width; ++col) {
+            cout << '*';
+        }
+        cout << endl;
+    }
 }
